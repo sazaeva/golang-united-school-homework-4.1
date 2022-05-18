9c44b34b95b7af55fb23ad335ce7b0ea73de0ad4
sazaeva <janylsazaeva@gmail.com>
2022-05-18 14:11:42 +0300

    reverse

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,10 @@
 package reverse_string
 
-func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+func ReverseString(input string) string {
+	output := []rune(input)
+	for i, j := 0, len(output)-1; i < j; i, j = i+1, j-1 {
+		output[i], output[j] = output[j], output[i]
+	}
+
+	return string(output)
 }
